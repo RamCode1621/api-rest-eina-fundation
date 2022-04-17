72c46e3a9d61cfc0aeb8e69a93df596435cfe584
Richard Aguilar <richard.aguilar.2020@hotmail.com>
2022-04-17 09:50:49 -0500

    unenable file bcrypt.js

--- src/functions/bcrypt.js
+++ src/functions/bcrypt.js
@@ -1,8 +1,8 @@
-const bcrypt=require('bcryptjs')
-let salt=bcrypt.genSaltSync(10)
+// const bcrypt=require('bcryptjs')
+// let salt=bcrypt.genSaltSync(10)
 
-const encryptPassword=async(password)=>{
-    return await bcrypt.hash(password,salt)
-}
+// const encryptPassword=async(password)=>{
+    // return await bcrypt.hash(password,salt)
+// }
 
-module.exports=encryptPassword
+// module.exports=encryptPassword
