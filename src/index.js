90005663ad2f6177923b507f628b3a8302dd3c35
Richard Aguilar <richard.aguilar.2020@hotmail.com>
2022-04-17 00:26:07 -0500

    configure port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config('../')
 const app=require('./server')
 
-const port=process.env.PORT
+const port=process.env.PORT || 3333
 
 app.listen(port,()=>{
     console.log(`Server on port ${port}`)
