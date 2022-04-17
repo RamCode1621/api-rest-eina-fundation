89a18dc5fad60704fe9a4de4136fd044d54c4bcb
Richard Aguilar <richard.aguilar.2020@hotmail.com>
2022-04-17 00:18:50 -0500

    added fake endpoint

--- src/routes/Sesiones.routes.js
+++ src/routes/Sesiones.routes.js
@@ -4,5 +4,7 @@ const router=Router()
 const {authentication}=require('../controllers/Auth.controller')
 
 router.post('/api/v1/post/auth',authentication)
-
+router.get('/api',(req,res)=>{
+    res.send('Hola mundo')
+})
 module.exports=router
