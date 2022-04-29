54ea2c33d2f072369fab7067fc044275d89195d8
Ubuntu <ubuntu@ip-172-31-31-177.ec2.internal>
2022-04-29 00:06:06 +0000

    edit package lock

---
+++ config.pm2.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps : [{
+    name        : "Eina fundation server",
+    script      : "src/index.js",
+    watch       : true,
+    merge_logs  : true,
+    cwd         : "/home/ubuntu/api-rest-eina-fundation",
+   }
+}
